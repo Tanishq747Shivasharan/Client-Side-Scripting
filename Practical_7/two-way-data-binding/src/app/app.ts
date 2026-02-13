d55da883ee66a0ebf6a414b869a992718d256530
Aryan Jakkal <aryanjakkal717@gmail.com>
2026-02-13 17:21:15 +0530

    Added Practical7

---
+++ Practical_7/two-way-data-binding/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  name = "";
+
+}
