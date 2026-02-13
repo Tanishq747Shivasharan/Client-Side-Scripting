0d971bd2be7dcd416ba37dac23e945e0ad36f27a
Aryan Jakkal <aryanjakkal717@gmail.com>
2026-02-13 16:00:14 +0530

    Added Practical 6

---
+++ Practical_6/data-model-view/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  Firstname = "Aryan";
+  Lastname = "Jakkal";
+}
