a2a275bd58c2349469dcb70c8a5a73ba3bd07bda
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-02-23 22:04:18 +0530

    Added an beutifull employee registration form.

--- Practical_05/employee-app/src/app/app.ts
+++ Practical_05/employee-app/src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('employee-app');
+
+  employeeForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+
+    this.employeeForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', [Validators.required, Validators.min(18), Validators.max(60)]],
+      gender: [''],
+      department: ['', Validators.required],
+      skills: this.fb.group({
+        java: [false],
+        python: [false],
+        angular: [false]
+      }),
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.employeeForm.valid) {
+      alert("Employee Registered Successfully!");
+      console.log(this.employeeForm.value);
+    }
+  }
 }
