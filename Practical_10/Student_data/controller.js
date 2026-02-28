f778a8731158ef9abf66d3fbb5292ec4a419a69b
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-02-28 21:00:16 +0530

    Added the practical realted question number 1 to Practical number 10.

---
+++ Practical_10/Student_data/controller.js
@@ -0,0 +1,14 @@
+app.controller("studCtrl", function($scope){
+    $scope.students=[
+        {roll:1, name:"Amit", marks:85},
+        {roll:2, name:"Riya", marks:95},
+        {roll:3, name:"Tanishq", marks:98},
+        {roll:4, name:"Arya", marks:84},
+        {roll:5, name:"Vinayak", marks:85},
+        {roll:6, name:"Aryan", marks:82},
+        {roll:7, name:"Dhariyashil", marks:86},
+        {roll:8, name:"Pooja", marks:90},
+        {roll:9, name:"Arav", marks:81},
+        {roll:10, name:"Sumit", marks:95}
+    ]
+});
