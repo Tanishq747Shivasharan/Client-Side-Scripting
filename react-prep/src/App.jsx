85cb52aae700bdc01aeb67c99ad4145ea87178fc
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-04-07 00:06:45 +0530

    Experimented with react hooks

--- react-prep/src/App.jsx
+++ react-prep/src/App.jsx
@@ -1,32 +1,40 @@
 // Import useState Hook to store and update data
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Create main component 
 function App() {
   
   // Create state variable userName, initially empty
-  const [userName, setUserName] = useState("")
+  const [count, setCount] = useState(0)
 
-  // Function to handle input change
-  function handleInputChange() {
-    setUserName(event.target.value)
+  useEffect(function() {
+    document.title = `Count: ${count}`
+  },[count])
+
+  // Function to handle increment button
+  function handleIncrement() {
+    setCount(count + 1)
+  }
+  
+  // Function to handle decrement button
+  function handleDecrement() {
+    if (count == 0){
+      setCount(count)
+    }
+    else{
+      setCount(count - 1)
+    }
   }
   
   // return statement to return jsx to the page
   return (
     <div>
-      <h2>React Form Example Pr14</h2>
+      <h2>Counter app React States</h2>
+
+      <h1>{count}</h1>
       
-      <input 
-        type="text"
-        value={userName}
-        onChange={handleInputChange}
-        placeholder="Enter your name"  
-      />
-
-      <br /><br />
-
-      <p>Hello, {userName}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 
